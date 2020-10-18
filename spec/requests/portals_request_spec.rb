86f2e00f102ae892cc1cdfc300276c004908f6b4
Vikram Panmand <vikky.panmand@gmail.com>
2020-10-18 20:42:26 +0530

    changes made to check authenticated request

--- spec/requests/portals_request_spec.rb
+++ spec/requests/portals_request_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Portals" do
-	describe "GET sessions#new" do
-		it "signs user in" do
+
+	describe "GET Portals#index" do
+		it "return list of users" do
+
+			#factory to generate user
 			user = create(:user)
-			
+
+			# check authenticated user
 			sign_in user
 			get root_path
-			expect(controller.current_user).to_not eq(nil)
-		end
+			
+			#get all users by calling portals index
+			get portals_path
 
-		it "get users data" do
-
+			#check response from /portals
+			expect(response).to be_successful 
 		end
 	end
+
 end
--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -2,15 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Sessions" do
 	it "signs user in and out" do
+		
+		#factory to generate user
 		user = create(:user)
 		
+		#sign in check
 		sign_in user
 		get root_path
 		expect(controller.current_user).to_not eq(nil)
 		
+		#sign out check
 		sign_out user
 		get root_path
 		expect(controller.current_user).to be_nil
 	end
-	
+
 end
