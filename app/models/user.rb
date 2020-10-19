6fd10990629312df3569ffdb6122b3049a523723
Vikram Panmand <vikky.panmand@gmail.com>
2020-10-19 13:25:58 +0530

    added tooltip and password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :rememberable
   devise :database_authenticatable, :registerable, :recoverable, :validatable
+  
+  validate :validate_password
+  
 
   enum role: [:admin, :user]
 
 
   def set_role(role)
   	update(role: role)
+  end
+
+  def validate_password
+  	reg = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}\z/
+  	unless reg.match(password)
+  		errors.add(:password, "Password contains minimum eight characters, at least one uppercase letter, one lowercase letter and one number")
+  	end
   end
 end
